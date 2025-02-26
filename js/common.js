18a87195a9061b1e1bdf218cf78e37194b1a9cd0
wb96choi <choi4601280@gmail.com>
2025-02-26 12:49:37 +0900

    드래그기능 추가 및 보완

--- js/common.js
+++ js/common.js
@@ -10,6 +10,20 @@ $(document).ready(function () {
         $.getScript('js/drag.js');
     }
 
+    // 스크롤
+    var navBtn = $('nav button');
+    $(navBtn).on('click', function () {
+        var scrollTarget = $(this).attr('data-target');
+        var scrollPosition = $(scrollTarget).offset().top;
+
+        if (scrollTarget === '#sec01') {
+            scrollPosition -= 53;
+        };
+
+        $('html, body').animate({
+            scrollTop: scrollPosition
+        }, 'smooth');
+    });
 });
 
 
--- js/drag.js
+++ js/drag.js
@@ -1,10 +1,55 @@
 $(document).ready(function () {
-    $(".sticker").draggable();
-    $(".drop-area").droppable({
-        drop: function(event, ui) {
-            $(this).addClass("highlight");
-        }
+    var dropArea = $(".drop_area");
+    var dragItem = $(".drag_item");
+    var originalPosition = dragItem.position();
+
+    dragItem.on('mousedown', function (e){
+        var offsetX = e.clientX - dragItem.position().left;
+        var offsetY = e.clientY - dragItem.position().top;
+        $(document).on('mousemove', function(e){
+            dragItem.css({
+                top: e.clientY - offsetY,
+                left: e.clientX - offsetX
+            });
+        });
+        $(document).on('mouseup', function(){
+            $(document).off('mousemove');
+
+
+
+            
+            if (isInsideDropArea(dragItem, dropArea)){
+                dragItem.addClass('dropped');
+                dragItem.appendTo(dropArea);
+                dragItem.css({
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%'
+                });
+            } else {
+                dragItem.removeClass('dropped');
+                dragItem.animate(originalPosition, 'fast');
+            }
+        });
     });
+
+    function isInsideDropArea(dragItem, dropArea){
+        var dropAreaOffset = dropArea.offset();
+        var dropRight = dropAreaOffset.left + dropArea.outerWidth();
+        var dropBottom = dropAreaOffset.top + dropArea.outerHeight();
+        var dragItemOffset = dragItem.offset();
+        var dragRight = dragItemOffset.left + dragItem.outerWidth();
+        var dragBottom = dragItemOffset.top + dragItem.outerHeight();
+
+        return !(
+            dragRight < dropAreaOffset.left ||
+            dragItemOffset.left > dropRight ||
+            dragBottom < dropAreaOffset.top ||
+            dragItemOffset.top > dropBottom
+        )
+    };
 
 
     console.log("drag.js loaded");
